ad2f319c13eb45877a4bfa952d55fb4fc1cb53a2
khaixa <n8tennyson@gmail.com>
2024-02-15 19:39:34 -0500

    working buttons

--- static/script.js
+++ static/script.js
@@ -1,18 +1,26 @@
 //Frontend
 //Web Music Player
+const btnEl = document.querySelector('.btn')
 
 function play(){
+    console.log()
     var audio = document.getElementById("audioPlayer");
     audio.play();
 }
 
 function pause(){
+    console.log()
     var audio = document.getElementById("audioPlayer");
     audio.pause();
 }
 
 function stop(){
+    console.log()
     var audio = document.getElementById("audioPlayer");
     audio.pause();
     audio.currentTime = 0;
 }
+
+btnEl.addEventListener('click', play)
+btnEl.addEventListener('click', pause)
+btnEl.addEventListener('click', stop)
