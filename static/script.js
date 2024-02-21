58d8df8f5b9cbe73be13bb9fc82873f07ef810b3
khaixa <n8tennyson@gmail.com>
2024-02-21 15:06:19 -0500

    022124

--- static/script.js
+++ static/script.js
@@ -1,33 +1,54 @@
 //Frontend
 //Web Music Player
 const btnEl = document.querySelector('.btn')
-
-function playPause() {
-    var audio = document.getElementById("audioPlayer");
-    var button = document.getElementById("playPauseButton");
+const audio = document.getElementById("audioPlayer");
+const currentTimeElement = document.getElementById("currentTime");
+const totalTimeElement = document.getElementById("totalTime");
     
-    if (audio.paused) {
-        audio.play();
-        button.innerHTML = "Pause";
-    } else {
-        audio.pause();
-        button.innerHTML = "Play";
+    audio.addEventListener("loadmetadata", function(){
+        var duration = audio.duration;
+        progressBar.max = duration
+        updateTimeDisplay();
+    });
+
+    audio.addEventListener("timeupdate", function(){
+        var currentTime = audio.currentTime;
+        progressBar.value = currentTime;
+        updateTimeDisplay();
+    })
+
+    function updateTimeDisplay(){
+        currentTimeElement.textContent = formatTime(audio.currentTime);
+        totalTimeElement.textContent = formatTime(audio.duration);
     }
-}
+
+    function formatTime(time) {
+        var minutes = Math.floor(time / 60);
+        var seconds = Math.floor(time % 60);
+        return minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
+    }
+
+    function playPause() {
+        var audio = document.getElementById("audioPlayer");
+        var button = document.getElementById("playPauseButton");
+        
+        if (audio.paused) {
+            audio.play();
+            button.innerHTML = "Pause";
+        } else {
+            audio.pause();
+            button.innerHTML = "Play";
+        }
+    }
 
 
-function stop(){
-    console.log()
-    var audio = document.getElementById("audioPlayer");
-    audio.pause();
-    audio.currentTime = 0;
-}
+    function stop(){
+        console.log()
+        var audio = document.getElementById("audioPlayer");
+        audio.pause();
+        audio.currentTime = 0;
+    }
 
-//document.addEventListener("DOMContentLoaded", function () {
-//    var progressBar = document.getElementById("progressBar");
-
-//}
-
-btnEl.addEventListener('click', play)
-btnEl.addEventListener('click', pause)
-btnEl.addEventListener('click', stop)
+    btnEl.addEventListener('click', play)
+    btnEl.addEventListener('click', pause)
+    btnEl.addEventListener('click', stop)
